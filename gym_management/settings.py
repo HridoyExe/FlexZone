09f0c7ae8921dcf913b8f11ac836b33dd7e42c06
Md Forman Ali <formanislamhridoy@gmail.com>
2026-02-13 16:26:30 +0600

    Cros header bad

--- gym_management/settings.py
+++ gym_management/settings.py
@@ -22,7 +22,6 @@ AUTH_USER_MODEL = "users.User"
 
 # Application definition
 INSTALLED_APPS = [
-    "corsheaders",
     "whitenoise.runserver_nostatic",
     'django.contrib.admin',
     'django.contrib.auth',
@@ -42,7 +41,6 @@ INSTALLED_APPS = [
 
 
 MIDDLEWARE = [
-     "corsheaders.middleware.CorsMiddleware",
     "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.security.SecurityMiddleware',
     "whitenoise.middleware.WhiteNoiseMiddleware",
@@ -53,10 +51,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-CORS_ALLOWED_ORIGINS = [
+# CORS_ALLOWED_ORIGINS = [
     
-    "http://localhost:5173",           
-]
+#     "http://localhost:5173",           
+# ]
 
 ROOT_URLCONF = 'gym_management.urls'
 
