da32e80c3345a8feedea8516872356eef6270a40
Md Forman Ali <formanislamhridoy@gmail.com>
2026-02-15 23:49:56 +0600

    env issue

--- gym_management/settings.py
+++ gym_management/settings.py
@@ -171,8 +171,8 @@ SIMPLE_JWT ={
 }
 
 DJOSER = {
-    'EMAIL_FRONTEND_PROTOCOL' : config('FRONTEND_PROTOCOL'),
-    'EMAIL_FRONTEND_DOMAIN' : config('FRONTEND_DOMAIN'),
+    'EMAIL_FRONTEND_PROTOCOL' : config('FRONTEND_PROTOCOL', default='http'),
+    'EMAIL_FRONTEND_DOMAIN' : config('FRONTEND_DOMAIN', default='localhost:5173'),
     'EMAIL_FRONTEND_SITE_NAME' :"FlexZone",
     'PASSWORD_RESET_CONFIRM_URL': 'password/reset/confirm/{uid}/{token}',
     'LOGIN_FIELD': 'email',
@@ -204,8 +204,8 @@ SWAGGER_SETTINGS = {
     }
 }
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
-EMAIL_PORT = config('EMAIL_PORT')
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
